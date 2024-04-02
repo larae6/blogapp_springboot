3eb1f16cc5903459cde831a31ef23b3b8ac683b9
larae6 <laralara359@icloud.com>
2024-04-01 17:17:31 -0700

    initial project setup

---
+++ src/test/java/com/atlas/blogapp_springboot/BlogappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.blogapp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
